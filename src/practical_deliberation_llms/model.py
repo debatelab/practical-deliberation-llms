eed141da57b24cf29133ddaf540524f81698312a
Gregor Betz <3662782+ggbetz@users.noreply.github.com>
2026-02-02 14:07:57 +0100

    Update model.py

--- src/practical_deliberation_llms/model.py
+++ src/practical_deliberation_llms/model.py
@@ -18,19 +18,15 @@ class PracticalProblem:
         return PracticalProblem(
             decision_situation=self.decision_situation,
             actions=self.actions[::-1],
-            labels=self.labels
+            labels=self.labels,
         )
 
     @staticmethod
     def options_list(labels, actions, **kwargs) -> str:
         """Formats options as string for prompt"""
         assert len(labels) == len(actions), "labels and options must have same length"
-        foptions = [
-            f"({label}) {option}"
-            for label, option in zip(labels, actions)
-        ]
-        return "\n".join(foptions)        
-    
+        foptions = [f"({label}) {option}" for label, option in zip(labels, actions)]
+        return "\n".join(foptions)
 
 
 @dataclasses.dataclass
@@ -43,4 +39,3 @@ class Choice:
         assert self.label is None and self.idx is None, "label and idx must be None"
         self.label = max(self.label_probs.items(), key=itemgetter(1))[0]
         self.idx = list(self.label_probs.keys()).index(self.label)
-
